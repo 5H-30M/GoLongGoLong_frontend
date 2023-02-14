92f57e644226cd777d2973bdc9180648c1b4135a
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-02-14 10:40:12 +0900

    [STYLE] GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
     <div>
+      <Router>
+        <GlobalStyle/>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    font-family:'NanumSquareNeo';
+    }
+`;
