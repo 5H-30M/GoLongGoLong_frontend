e56c3f4e04d428a5a30115cfcd8cfcd75a706c97
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-02-18 03:48:08 +0900

    [FEAT] MainAfterLoginPage 생성

--- src/pages/MainAfterLoginPage.tsx
+++ src/pages/MainAfterLoginPage.tsx
@@ -1,11 +1,32 @@
-import Slider from "components/mainAfterLogin/Slider";
+import styled from "styled-components";
+import Search from "components/Search";
+import Slider from "components/Slider";
+import Board from "components/Board";
 
 const MainAfterLoginPage = () => {
     return (
-        <>
+        <Container>
             <Slider />
-        </>
+            <InnerContainer>
+                <Search />
+                <Board />
+            </InnerContainer>
+        </Container>
     );
 };
 
 export default MainAfterLoginPage;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 50px;
+`;
+const InnerContainer = styled.div`
+    width: 1200px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`;
