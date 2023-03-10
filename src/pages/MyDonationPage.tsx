40ac7fdbc413076dbdf8ece757d131b17a2cad3d
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-03-10 20:13:26 +0900

    [FEAT] 기부현황 > 내 기부금 트래킹 생성

--- src/pages/MyDonationPage.tsx
+++ src/pages/MyDonationPage.tsx
@@ -6,8 +6,9 @@ import {
     RowDiv,
 } from "components/Common/MypageStyle";
 import Search from "components/Common/Search";
-import styled from "styled-components";
 import img1 from "../assets/imgs/test/1.jpg";
+import DonationTracking from "components/MyDonationPage/DonationTracking";
+import styled from "styled-components";
 
 const MyDonationPage = () => {
     return (
@@ -25,26 +26,36 @@ const MyDonationPage = () => {
                 </RowDiv>
 
                 <GreyDiv>
-                    <img src={img1}></img>
-                    <ColumnDiv>
-                        <text className="postTitle">
-                            다리를 다친 뚜비를 도와주세요
-                        </text>
-                        <text className="author">카라카라캐리</text>
-                        <li>후원일 2022. 11. 21</li>
-                        <li>32,000원 후원</li>
-                    </ColumnDiv>
+                    <ColumnDivForWrapping>
+                        <RowDivForWrapping>
+                            <img src={img1}></img>
+                            <ColumnDiv>
+                                <text className="postTitle">
+                                    다리를 다친 뚜비를 도와주세요
+                                </text>
+                                <text className="author">카라카라캐리</text>
+                                <li>후원일 2022. 11. 21</li>
+                                <li>32,000원 후원</li>
+                            </ColumnDiv>
+                        </RowDivForWrapping>
+                        <DonationTracking />
+                    </ColumnDivForWrapping>
                 </GreyDiv>
                 <GreyDiv>
-                    <img src={img1}></img>
-                    <ColumnDiv>
-                        <text className="postTitle">
-                            다리를 다친 뚜비를 도와주세요
-                        </text>
-                        <text className="author">카라카라캐리</text>
-                        <li>후원일 2022. 11. 21</li>
-                        <li>32,000원 후원</li>
-                    </ColumnDiv>
+                    <ColumnDivForWrapping>
+                        <RowDivForWrapping>
+                            <img src={img1}></img>
+                            <ColumnDiv>
+                                <text className="postTitle">
+                                    다리를 다친 뚜비를 도와주세요
+                                </text>
+                                <text className="author">카라카라캐리</text>
+                                <li>후원일 2022. 11. 21</li>
+                                <li>32,000원 후원</li>
+                            </ColumnDiv>
+                        </RowDivForWrapping>
+                        <DonationTracking />
+                    </ColumnDivForWrapping>
                 </GreyDiv>
             </Container>
         </Wrapper>
@@ -52,3 +63,16 @@ const MyDonationPage = () => {
 };
 
 export default MyDonationPage;
+
+const ColumnDivForWrapping = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+const RowDivForWrapping = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    gap: 24px;
+`;
