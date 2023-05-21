b2c48485ec771db67b943eb58867188174109759
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-05-22 02:32:10 +0900

    [FEAT] iamport 라이브러리 사용을 위한 Window interface 생성, 새로고침시 access token 검사 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import { useAppDispatch } from "hooks/useAppDispatch";
@@ -16,45 +16,20 @@ import EpiloguePost from "pages/EpiloguePost/EpiloguePost";
 import { fetchDonaPosts, fetchEpilPosts } from "redux/postSlice";
 import PostingDonation from "pages/PostingDonation/PostingDonation";
 import PostingEpilogue from "pages/PostingEpilogue/PostingEpilogue";
-import { userType, postType } from "utils/types";
 import MyPage from "pages/MyPage/MyPage";
+import ReceiptOCR from "pages/ReceiptOCR/ReceiptOCR";
+import ChargeToken from "pages/ChargeToken/ChargeToken";
+import Oauth from "components/Login/Oauth";
+import { fetchUserInfo } from "redux/authSlice";
+import { GetKakaoUserApi } from "api/userInfo";
+import { RefreshTokenApi } from "api/auth";
+import CreateWallet from "utils/web3/CreateWallet";
 
-//임시 데이터
-const post: postType = {
-    amount: 500000,
-    content: "",
-    images: [
-        "https://pbs.twimg.com/profile_images/1450121710674407426/SwOINBDw_400x400.jpg",
-    ],
-    period: 10,
-    plans: {},
-    target_amount: 500000,
-    title: "",
-    post_id: 20,
-    uploader_id: -1,
-    raised_people: 100,
-    created_at: "2023-04-10",
-    status: 4,
-};
-export const testUser: userType = {
-    user_id: 0,
-    donations: [
-        { post_id: 20, donated_at: "2023-04-22", amount: 1000, trx_id: "none" },
-        { post_id: 20, donated_at: "2023-04-22", amount: 1000, trx_id: "none" },
-    ],
-    interests: [20],
-    notifications: [
-        { post_id: 20, is_check: false, status: 3, tag: true },
-        { post_id: 20, is_check: false, status: 4, tag: false },
-    ],
-    fundraisings: [post, post],
-    profile_img_url:
-        "https://pbs.twimg.com/profile_images/1450121710674407426/SwOINBDw_400x400.jpg",
-    token_amount: 5000,
-    nickname: "nyaaaa",
-    sns_email: "nyaaaa@gmail.com",
-    created_at: "2023-04-23",
-};
+declare global {
+    interface Window {
+        IMP: any;
+    }
+}
 
 function App() {
     //api를 통해 post 정보를 가져와 리덕스에 저장
@@ -64,14 +39,35 @@ function App() {
         dispatch(fetchEpilPosts());
     }, [dispatch]);
 
+    //access token이 있는 경우, 갱신한 뒤 사용자 정보를 리덕스에 저장한다.
+    useEffect(() => {
+        const getUser = async () => {
+            //토큰 갱신
+            let token = await RefreshTokenApi();
+
+            //사용자 정보 리덕스 저장
+            let { data } = await GetKakaoUserApi({ token });
+            const email = data.kakao_account.email;
+
+            dispatch(fetchUserInfo({ email }));
+        };
+
+        const token = window.localStorage.getItem("accessToken");
+        if (token) {
+            getUser();
+        }
+    }, [dispatch]);
+
     return (
         <div>
             <Router>
                 <GlobalStyle />
+                <CreateWallet password={"hi"} />
                 <Header />
                 <Routes>
                     <Route path="/" element={<MainAfterLogin />}></Route>
                     <Route path="/login" element={<LoginPage />}></Route>
+                    <Route path="/oauth" element={<Oauth />}></Route>
                     <Route path="/epilogue" element={<Epilogue />}></Route>
                     <Route path="/post/:id" element={<DonationPost />}></Route>
                     <Route
@@ -91,9 +87,11 @@ function App() {
                         element={<PostingDonation />}
                     />
                     <Route
-                        path="/posting/epilogue"
+                        path="/posting/epilogue/:id"
                         element={<PostingEpilogue />}
                     />
+                    <Route path="/receiptOCR/:id" element={<ReceiptOCR />} />
+                    <Route path="/charge" element={<ChargeToken />} />
                 </Routes>
                 <Footer />
             </Router>
