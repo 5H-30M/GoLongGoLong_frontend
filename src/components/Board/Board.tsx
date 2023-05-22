f75ff7788fc536bd2b7aa73a6cc5970e91eca99e
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-05-22 23:58:34 +0900

    [FIX] posts 변수가 배열 타입인지 확인 후 map 함수 사용

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -21,7 +21,6 @@ const Board = ({ kindOfCard, donaPosts, epilPosts }: propsType): any => {
     const filteredBy = useAppSelector((state) => state.post.filteredBy);
     const user = useAppSelector((state) => state.auth.userData);
     const region = user.region;
-
     const renderCard = () => {
         //kindOfCard props가 "donation"일 경우 <DonationsCard> 컴포넌트를, 아닐 경우 <EpilogueCard> 컴포넌트를 렌더한다.
         const result: any[] = [];
@@ -34,50 +33,58 @@ const Board = ({ kindOfCard, donaPosts, epilPosts }: propsType): any => {
                     region,
                 });
 
+                //사용자 지역의 모금글이 없는 경우
                 if (donaPosts.length == 0 && filteredBy == "region") {
                     result.push(
                         <NoPostings notice={"❗우리 지역의 모금글이 없어요"} />
                     );
                 }
-
-                result.push(
-                    donaPosts?.map((item, index) => {
-                        return (
-                            <Link
-                                key={index}
-                                to={`/post/${item.post_id}`}
-                                style={{
-                                    color: "black",
-                                    textDecoration: "none",
-                                }}
-                            >
-                                <DonationCard post={item} />
-                            </Link>
-                        );
-                    })
-                );
+                if (Array.isArray(donaPosts)) {
+                    result.push(
+                        donaPosts.map((item, index) => {
+                            return (
+                                <Link
+                                    key={index}
+                                    to={`/post/${item.post_id}`}
+                                    style={{
+                                        color: "black",
+                                        textDecoration: "none",
+                                    }}
+                                >
+                                    <DonationCard post={item} />
+                                </Link>
+                            );
+                        })
+                    );
+                } else {
+                    return <Spinner />;
+                }
             } else {
                 result.push(<NoPostings notice={"❗아직 글이 없어요"} />);
             }
         }
         if (kindOfCard == "epilogue") {
             if (epilPosts && epilPosts.length > 0) {
-                result.push(
-                    epilPosts?.map((item, index) => {
-                        return (
-                            <Link
-                                key={index}
-                                to={`/epilogue/post/${item.id}`}
-                                style={{
-                                    color: "black",
-                                    textDecoration: "none",
-                                }}
-                            >
-                                <EpilogueCard epilpost={item} />
-                            </Link>
-                        );
-                    })
-                );
+                if (Array.isArray(epilPosts)) {
+                    result.push(
+                        epilPosts.map((item, index) => {
+                            return (
+                                <Link
+                                    key={index}
+                                    to={`/epilogue/post/${item.id}`}
+                                    style={{
+                                        color: "black",
+                                        textDecoration: "none",
+                                    }}
+                                >
+                                    <EpilogueCard epilpost={item} />
+                                </Link>
+                            );
+                        })
+                    );
+                } else {
+                    return <Spinner />;
+                }
             } else {
                 result.push(<NoPostings notice={"❗아직 글이 없어요"} />);
             }
