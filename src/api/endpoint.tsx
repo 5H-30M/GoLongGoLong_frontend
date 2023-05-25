aa25e8287088ad09375c6cb2a2bcef58135f0a76
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-05-25 13:18:44 +0900

    [FIX] 임시로 apiEndpoint에 백엔드 서버 주소를 바로 대입

--- src/api/endpoint.tsx
+++ src/api/endpoint.tsx
@@ -1,1 +1,2 @@
-export const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+//export const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
+export const apiEndpoint = "http://3.25.60.61:8080";
