b29c266bd26a229edf524cc8b3afe34ad0d38b7f
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-05-25 13:29:10 +0900

    [FIX] apiEndpoint에 환경 변수 대입

--- src/api/endpoint.tsx
+++ src/api/endpoint.tsx
@@ -1,2 +1,1 @@
-//export const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
-export const apiEndpoint = "http://3.25.60.61:8080";
+export const apiEndpoint = process.env.REACT_APP_API_ENDPOINT;
