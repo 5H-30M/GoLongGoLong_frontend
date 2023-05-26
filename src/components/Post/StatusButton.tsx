629c5115d0c7fc3bda82844daf761ddc26d33ab1
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-05-26 12:08:10 +0900

    [FEAT] 변경된 status에 맞게 로직 수정

--- src/components/Post/StatusButton.tsx
+++ src/components/Post/StatusButton.tsx
@@ -1,8 +1,10 @@
 import { GreyButton } from "components/Common/ButtonStyle";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { postType } from "utils/types";
+import { postType, userType_new } from "utils/types";
 import DonateModal from "components/DonateModal/DonateModal";
+import { ViewApi } from "api/member";
+import { useAppSelector } from "hooks/useAppSelector";
 
 interface propsType {
     post: postType;
@@ -10,16 +12,11 @@ interface propsType {
 
 const StatusButton = ({ post }: propsType) => {
     const navigate = useNavigate();
-    const [isWriter, setIsWriter] = useState<boolean>(true);
+    const user_id = window.localStorage.getItem("userId");
+    const userId = user_id ? parseInt(user_id) : 0;
+    const writerId = post.uploader_id;
+
     const [renderModal, setRenderModal] = useState<boolean>(false);
-    // useEffect(()=>{
-    //     const getUser=()=>{
-    //         if user_id == post.uploader_id{
-    //             setIsWriter(true);
-    //         }
-    //     }
-    //     getUser();
-    // }, [])
 
     const handleStatus0 = () => {
         setRenderModal(!renderModal);
@@ -32,43 +29,46 @@ const StatusButton = ({ post }: propsType) => {
     };
     const handleStatus = () => {
         let result: React.ReactElement = <></>;
-
-        switch (post.status) {
-            case 0:
-                result = (
-                    <GreyButton onClick={handleStatus0}>기부하기</GreyButton>
-                );
-                break;
-            case 1:
-            case 4:
-                result = <GreyButton>모금종료</GreyButton>;
-                break;
-            case 2:
-                if (isWriter) {
+        if (userId) {
+            switch (post.status) {
+                case 0:
                     result = (
-                        <GreyButton onClick={handleStatus2}>
-                            토큰 수령하기
+                        <GreyButton onClick={handleStatus0}>
+                            기부하기
                         </GreyButton>
                     );
-                } else result = <GreyButton>모금종료</GreyButton>;
-                break;
-            case 3:
-                if (isWriter) {
-                    result = (
-                        <GreyButton onClick={handleStatus3}>
-                            후기 작성하기
-                        </GreyButton>
-                    );
-                } else result = <GreyButton>모금종료</GreyButton>;
-                break;
+                    break;
+                case 1:
+                    if (userId == writerId) {
+                        result = (
+                            <GreyButton onClick={handleStatus2}>
+                                토큰 수령하기
+                            </GreyButton>
+                        );
+                    } else result = <GreyButton>모금종료</GreyButton>;
+                    break;
+                case 2:
+                    if (userId == writerId) {
+                        result = (
+                            <GreyButton onClick={handleStatus3}>
+                                후기 작성하기
+                            </GreyButton>
+                        );
+                    } else result = <GreyButton>모금종료</GreyButton>;
+                    break;
+                case 3:
+                    result = <GreyButton>모금종료</GreyButton>;
+                    break;
+            }
         }
+
         return result;
     };
 
     return (
         <>
             {handleStatus()}
-            {renderModal && <DonateModal />}
+            {renderModal && <DonateModal postId={post.post_id} />}
         </>
     );
 };
