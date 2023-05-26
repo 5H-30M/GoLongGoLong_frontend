b8d9c360f486a919db3b8784bb4cba7ca903608d
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-05-26 11:51:58 +0900

    [FEAT] tx modal 수정

--- src/ShowTxModal/ShowTxModal.tsx
+++ src/ShowTxModal/ShowTxModal.tsx
@@ -8,15 +8,77 @@ import {
     Content,
     Button,
 } from "./Style";
-import { ShowTxDetail } from "utils/web3/ShowTxDetail";
+import { transitionType } from "utils/types";
 
 interface propsType {
-    txId: string;
+    tx: transitionType;
     setModal: React.Dispatch<React.SetStateAction<boolean>>;
+    status: number;
 }
 
-const ShowTxModal = ({ txId, setModal }: propsType) => {
-    const tx = ShowTxDetail({ txId });
+const ShowTxModal = ({ tx, setModal, status }: propsType) => {
+    const renderInfo = () => {
+        const result: JSX.Element[] = [];
+        if (status == 1) {
+            result.push(
+                <>
+                    <TextBox>
+                        <Name>모금글 지갑 주소</Name>
+                        <Content>{tx.toAddress}</Content>
+                    </TextBox>
+                    <TextBox>
+                        <Name>지갑 트랜잭션 Etherscan 링크</Name>
+                        <Content>
+                            <a
+                                href={`http://sepolia.etherscan.io/address/${tx.toAddress}`}
+                            >
+                                http://sepolia.etherscan.io/address/
+                                {tx.toAddress}
+                            </a>
+                        </Content>
+                    </TextBox>
+                </>
+            );
+        } else {
+            result.push(
+                <>
+                    <TextBox>
+                        <Name>트랜잭션 ID</Name>
+                        <Content>{tx.transactionId}</Content>
+                    </TextBox>
+                    <TextBox>
+                        <Name>생성 시간</Name>
+                        <Content>{tx.transactionCreatedAt}</Content>
+                    </TextBox>
+                    <TextBox>
+                        <Name>전송자 지갑 주소</Name>
+                        <Content>{tx.fromAddress}</Content>
+                    </TextBox>
+                    <TextBox>
+                        <Name>수신자 지갑 주소</Name>
+                        <Content>{tx.toAddress}</Content>
+                    </TextBox>
+                    <TextBox>
+                        <Name>전송된 토큰 값</Name>
+                        <Content>{tx.amount}&nbsp; GOL</Content>
+                    </TextBox>
+                    <TextBox>
+                        <Name>Etherscan 링크</Name>
+                        <Content>
+                            <a
+                                href={`http://sepolia.etherscan.io/tx/${tx.transactionId}`}
+                            >
+                                http://sepolia.etherscan.io/tx/
+                                {tx.transactionId}
+                            </a>
+                        </Content>
+                    </TextBox>
+                </>
+            );
+        }
+        return result;
+    };
+
     return (
         <Container>
             <Overlay>
@@ -25,40 +87,7 @@ const ShowTxModal = ({ txId, setModal }: propsType) => {
                         트랜잭션 정보
                     </text>
                     <Column style={{ gap: "28px", width: "100%" }}>
-                        <TextBox>
-                            <Name>트랜잭션 ID</Name>
-                            <Content>{txId}</Content>
-                        </TextBox>
-                        <TextBox>
-                            <Name>생성 시간</Name>
-                            <Content>May-17-2023 02:42:36 PM +UTC</Content>
-                        </TextBox>
-                        <TextBox>
-                            <Name>전송자 지갑 주소</Name>
-                            <Content>
-                                0x54142a81f139e00d0bb21f1866cd66ede0b092bd
-                            </Content>
-                        </TextBox>
-                        <TextBox>
-                            <Name>수신자 지갑 주소</Name>
-                            <Content>
-                                0xb248d4cbb7c728ef0e81de0b4b17734aeed2d280
-                            </Content>
-                        </TextBox>
-                        <TextBox>
-                            <Name>전송된 토큰 값</Name>
-                            <Content>2000 GOL</Content>
-                        </TextBox>
-                        <TextBox>
-                            <Name>Etherscan 링크</Name>
-                            <Content>
-                                <a
-                                    href={`https://sepolia.etherscan.io/tx/${txId}`}
-                                >
-                                    https://sepolia.etherscan.io/tx/{txId}
-                                </a>
-                            </Content>
-                        </TextBox>
+                        {renderInfo()}
                     </Column>
                     <Button onClick={() => setModal(false)}>확인</Button>
                 </Box>
